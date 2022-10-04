2303adfeae7cdb788688d8d9b9d3100ab5399f80
Vinay Kumar <101393405+vinaykumar2n@users.noreply.github.com>
2022-10-04 16:19:46 +0530

    updated portfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Dreamer"],
+    strings: ["Java Back-End Developer", "Dreamer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Back-End Developer", "Dreamer"],
+    strings: ["Java Back-End Developer", "Dreamer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
